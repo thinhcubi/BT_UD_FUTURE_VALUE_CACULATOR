d519bb2d6731c7c8a920013645102ea6b9bc9b8d
thinhcubi <thinhcubi27@gmail.com>
2021-05-07 10:19:34 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <input type="text" name="Amount" placeholder="Luong tien dau tu ban dau"/>
+    <input type="text" name="Rate" placeholder="Lai suat nam"/>
+  <input type="submit" name="submit" value="Tinh" />
+</form>
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $Money = $_POST["Amount"];
+    $Interest = $_POST["Rate"];
+    $Future = $Money + ($Money * $Interest);
+    echo  $Future;
+}
+?>
+
+
+</body>
+</html>
